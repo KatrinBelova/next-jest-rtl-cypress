4ab70417ce39aa81184819154bee9da4c5b045af
KateBilova <kateryna.bilova@extrawest.com>
2023-03-09 23:46:25 +0200

    UI testing; mock service worker

---
+++ __tests__/__mocks__/msw/handlers.js
@@ -0,0 +1,32 @@
+import { rest } from 'msw';
+
+import { readFakeData } from '@/__tests__/__mocks__/fakeData';
+import { fakeUserReservations } from '@/__tests__/__mocks__/fakeData/userReservations';
+
+export const handlers = [
+  rest.get('http://localhost:3000/api/shows/:showId', async (req, res, ctx) => {
+    const { fakeShows } = await readFakeData();
+    const { showId } = req.params;
+
+    // Index / showID = 0 has seats available in fake data
+    // Index / showID = 1 has NO seats available
+
+    return res(ctx.json({ show: fakeShows[Number(showId)] }));
+  }),
+
+  rest.get(
+    `http://localhost:3000/api/users/:userId/reservations`,
+    async (req, res, ctx) => {
+      const { userId } = req.params;
+
+      // userId = 0 - empty list
+      // userId = 1 - data
+
+      return res(
+        ctx.json({
+          userReservations: Number(userId) === 1 ? fakeUserReservations : [],
+        })
+      );
+    }
+  ),
+];
